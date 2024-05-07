86ae0d2f56a6e1b6f5fd6ce4c22367b962496a78
Mike Jones <mikejones@yahoo.com>
2024-05-07 10:50:04 -0400

    attempt to render characters

--- planets.js
+++ planets.js
@@ -3,28 +3,59 @@
 // my code
 
 const baseUrl = `https://swapi2.azurewebsites.net/api`;
-const sp = new URLSearchParams(window.location.search);
-const id = sp.get('id');
-let planet = {};
+// let planet = {};
 let planetCharacters = [];
-let planetFilms = [];
+let characters =''
+let planetName = ''
+let climate = ''
+let population = ''
+let terrain = ''
 
-async function fetchPlanet(id){
-    planet = await fetch(`${baseUrl}/planets/${id}`).then(res =>res.json())
-    .then(response =>{if (response.length=== 0) {throw new Error('no more planets to display')}
-        else{
-            console.log(response)
-        }}
-    )
-    .catch(error => console.log(error))
+// from character file
+addEventListener('DOMContentLoaded', () => {
+
+    climate = document.querySelector('#climate')
+    planetName = document.querySelector('#name')
+    population = document.querySelector('#population')
+    terrain = document.querySelector('#terrain')
+    const sp = new URLSearchParams(window.location.search);
+    const id = sp.get('id');
+    getPlanet(id)
+    fetchCharacters(id)
+    
+  });
+
+  async function getPlanet(id) {
+    let planet;
+    try {
+      planet = await fetchPlanet(id)
+    //   character.homeworld = await fetchHomeworld(character)
+    //   character.films = await fetchFilms(character)
+    }
+    catch (ex) {
+      console.error(`Error reading character ${id} data.`, ex.message);
+    }
+    renderPlanet(planet);
 }
+
+// async function fetchPlanet(id){
+//     planet = await fetch(`${baseUrl}/planets/${id}`).then(res =>res.json())
+//     .then(response =>{if (response.length=== 0) {throw new Error('no more planets to display')}
+//         else{
+//             console.log(response)
+//         }}
+//     )
+//     .catch(error => console.log(error))
+// }
+async function fetchPlanet(id) {
+    let planetUrl = `${baseUrl}/planets/${id}`;
+    return await fetch(planetUrl)
+      .then(res => res.json())
+  }
 
 async function fetchCharacters(id){
     planetCharacters = await fetch(`${baseUrl}/planets/${id}/characters`).then(res =>res.json())
-    .then(response =>{if (response.length=== 0) {throw new Error('no more characters to display')}
-        else{
-            console.log(response)
-        }}
+    .then(response =>response.map(character=>planetCharacters.push(character))
     )
     .catch(error => console.log(error))
 }
@@ -39,4 +70,20 @@ async function fetchFilms(id){
     .catch(error => console.log(error))
 }
 
-fetchFilms(1)
+const renderPlanet = planet => {
+    characters = document.querySelector('#characters')
+    characters.innerHTML = planetCharacters.map(character=>console.log(`<span>${character}</span>`) )
+    planetName.innerHTML = planet.name
+    climate.innerHTML = planet.climate
+    population.innerHTML = planet.population
+    terrain.innerHTML = planet.terrain
+
+    // document.title = `SWAPI - ${character?.name}`;  // Just to make the browser tab say their name
+    // nameH1.textContent = character?.name;
+    // heightSpan.textContent = character?.height;
+    // massSpan.textContent = character?.mass;
+    // birthYearSpan.textContent = character?.birth_year;
+    // homeworldSpan.innerHTML = `<a href="/planets.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
+    // const filmsLis = character?.films?.map(film => `<li><a href="/films.html?id=${film.id}">${film.title}</li>`)
+    // filmsUl.innerHTML = filmsLis.join("");
+  }
